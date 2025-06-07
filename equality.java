9448c31a5649ea429f8a2fae4e009a9058eb8825
TAJWID AHMAD <tajwidahmad5@gmail.com>
2025-06-07 15:29:24 +0000

    equLITY of String

---
+++ equality.java
@@ -0,0 +1,13 @@
+public class equality  {
+    public static void main(String[] args) {
+        // note whenever we have to check the equaltity of the strings the will use s1.equals(s3)
+        String s1="tajwid";
+        String s2 = new String("ahmaad");
+        if (s1.equals(s2)) {
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
