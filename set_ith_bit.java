d7244a8887dfa269313e8586d61d87b0098ba456
TAJWID AHMAD <tajwidahmad5@gmail.com>
2025-06-10 16:10:56 +0000

    setting thting the ith bit of a binary

---
+++ set_ith_bit.java
@@ -0,0 +1,10 @@
+
+public class set_ith_bit {
+    public static void main(String[] args) {
+        int i=2;
+        int n=10;
+        int bitmask=1<<i;
+        System.out.println(n|bitmask);
+
+    }
+}
