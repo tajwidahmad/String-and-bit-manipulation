6ed8c6a99850e4b6295f0c65db09d0bddffaa883
TAJWID AHMAD <tajwidahmad5@gmail.com>
2025-06-10 16:08:15 +0000

    this progrs program checks the Strings if it is palindrome

---
+++ palindroime.java
@@ -0,0 +1,15 @@
+public class palindroime {
+    public static boolean checkpalindrome(String str) {
+        for(int i=0;i<str.length()/2;i++){
+            if (str.charAt(i)!=str.charAt(str.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "raecar";
+        System.out.println(checkpalindrome(str));
+    }
+}
+
